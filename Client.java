a5ee2d38dd3e2e5e3d46226ce50f6f32948c3a40
Nishu kumari <nishukumaridolly@gmail.com>
2025-03-13 03:50:50 +0530

    Client program file and Server program file

---
+++ Client.java
@@ -0,0 +1,89 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+class Client{
+    Socket socket;
+    BufferedReader bufferReadMsg;
+
+    PrintWriter sendMsg;
+    Client(){
+        System.out.println("Client started");
+        try{
+            System.out.println("Request sent to the server");
+            socket = new Socket("127.0.0.1",7777);/*this socket object is reffering to a specific server
+                                                                 has has been binded to that server*/
+
+            System.out.println("Client connected to the server");
+            bufferReadMsg = new BufferedReader(new InputStreamReader(socket.getInputStream())) ;/*return type of socket.getInputStream()
+                                                                             is InputStream ,and it read raw bytes from the input stream*/
+            sendMsg = new PrintWriter(socket.getOutputStream());
+           
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    void startWriting() {
+        Runnable r1 = () -> {
+            BufferedReader msg;
+            System.out.println("Writing started");
+            try {
+
+               while(!socket.isClosed()){
+                   msg= new BufferedReader(new InputStreamReader(System.in));
+                   String content = msg.readLine();
+                   sendMsg.println(content);
+                   sendMsg.flush();
+                   if(content.equals("exit")){
+                       socket.close();
+
+                   }
+               }
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+        };
+        new Thread(r1).start();
+
+    }
+    void startReading(){
+        Runnable r2 = ()->{                      //to see Runnable implementation press ctrl and click on Runnable
+            System.out.println("Reading started");
+            try{
+                while(!socket.isClosed()){
+                    String msg = bufferReadMsg.readLine();
+                    if(msg.equals("exit")){
+                        socket.close();
+
+                    }
+                    System.out.println("Server:"+msg);
+                }
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        };
+        new Thread(r2).start();
+
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.startWriting();
+        client.startReading();
+
+    }
+}
+
+
+/*
+Documentation
+if you close socket on client side then client's side socket will be closed it will not close socket on the server side
+but when server will try to send meg it will encounter IO Exception or SocketException ....the server should explicitly close the connection
+*/
---
+++ Server.java
@@ -0,0 +1,92 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/*
+Bidirectional Communication:
+
+Once the server has the input and output streams from the Socket, the server can read data from the client (via the InputStream)
+and write responses back to the client (via the OutputStream).
+The client's OutputStream (via its Socket object) is connected to the server's InputStream,
+and the client's InputStream is connected to the server's OutputStream.
+*/
+class Server {
+    ServerSocket serversocket;
+    Socket socket;
+    BufferedReader bufferReadMsg;
+    PrintWriter sendMsg;
+
+    Server() {
+        System.out.println("Server Started");
+        try {
+            serversocket = new ServerSocket(7777);
+            System.out.println("Waiting for client request");
+            socket = serversocket.accept();         /*here socket object has been binded to the specific client
+                                                    when server accepts request from a client ,server's inputStream binds with
+                                                     client's OutputStream and client's inputStream to server's OutputStream*/
+            bufferReadMsg = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            sendMsg = new PrintWriter(socket.getOutputStream());
+           
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    void startReading() {
+        Runnable r1 = () -> {
+            System.out.println("Reading started");
+            try {
+
+                while (!socket.isClosed()) {
+                    String msg = bufferReadMsg.readLine();
+                    if (msg.equals("exit")) {
+                        socket.close();
+
+                    }
+
+                    System.out.println("Client:" + msg);
+                }
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        };
+        new Thread(r1).start();
+
+    }
+
+
+    void startWriting() {
+        Runnable r2 = () -> {
+            BufferedReader Msg;
+            System.out.println("Writing started");
+            try {
+                while (!socket.isClosed()) {
+                    Msg = new BufferedReader(new InputStreamReader(System.in));
+                    String content = Msg.readLine();
+                    sendMsg.println(content);
+                    sendMsg.flush();
+                    if(content.equals("exit")){
+                        socket.close();
+
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+        };
+        new Thread(r2).start();
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.startReading();
+        server.startWriting();
+    }
+
+}
